d134ef4f01a656091cba710f07c3bc7787d6a378
AlGor032010 <139915545+AlGor032010@users.noreply.github.com>
2026-08-24 14:59:08 -0400

    Update deep.py

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,15 @@
+print("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 
+userSays = input()
+
+
+if userSays == "42":
+    print("Yes")
+elif userSays.strip(" ") == "42":
+    print("Yes")
+elif (userSays.strip().casefold() == "forty-two".strip().casefold() or
+    userSays.strip().casefold() == "forty two".strip().casefold()
+    ):
+    print("Yes")
+else:
+    print("No")
