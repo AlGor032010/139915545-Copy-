3322eeb93d9d8fd46cfcc3b64eae2f93295107ae
AlGor032010 <139915545+AlGor032010@users.noreply.github.com>
2026-08-24 15:01:03 -0400

    Update playback2.py

--- playback/playback2.py
+++ playback/playback2.py
@@ -1,1 +1,15 @@
+userInput = str(input("[Hey...how's...it...goin'...?]  "))
 
+wordCount = len(userInput.split())
+
+def reply1():
+   print('[Oh...? Tell...me...more...about...it...]')
+
+def reply2():
+    print('[Woah...! Slow...Down...!]')
+
+if wordCount == 1 or wordCount > 1 and "..." in userInput:
+    reply1()
+elif wordCount > 1 and " " in userInput:
+    reply2()
+    print("Sorry. " + userInput.replace(" ", "..."))
