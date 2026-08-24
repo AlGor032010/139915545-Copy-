963faf8b69ba359da13eb6aa1cbff2265db794a6
AlGor032010 <139915545+AlGor032010@users.noreply.github.com>
2026-08-24 15:00:01 -0400

    Update indoor.py

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,1 +1,12 @@
+#This program asks the user to use their indoor voice or thanks them for doing so
+sent = str(input("hello! how may i help you today? "))
 
+lowC = sent.lower()
+
+for char in sent:
+    if char.isupper():
+        print("shhhhhh! please use your indoor voice")
+        print(lowC)
+    else:
+        print("very well. thank you for using your indoor voice.")
+    break
