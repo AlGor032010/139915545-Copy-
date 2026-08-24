d4fbd018011ee6f56392e1b02779abd019a47f17
AlGor032010 <139915545+AlGor032010@users.noreply.github.com>
2026-08-24 14:58:49 -0400

    Update bank.py

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,12 @@
+greeting = input("Greeting: ")
 
+if (greeting.strip().casefold() == "hello".strip().casefold() or
+    greeting.casefold().startswith("hello".casefold())
+    ):
+    print("$0")
+elif (greeting.casefold().startswith("h".casefold()) and
+      greeting.casefold() != "hello".casefold()
+      ):
+    print("$20")
+else:
+    print("$100")
